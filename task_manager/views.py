894300ac26fa37e54189b784935110996adca5d1
Alireza <ali.r.matourianpoor@gmail.com>
2026-05-17 12:34:38 +0330

    add custom permission

--- task_manager/views.py
+++ task_manager/views.py
@@ -2,9 +2,11 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import TaskSerializer
 from .models import Task
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsCreatorOrReadOnly
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .pagination import TaskPagination
+from django.db.models import Q
 
 
 # Create your views here.
@@ -12,7 +14,7 @@ from .pagination import TaskPagination
 
 class TaskViewSet(ModelViewSet):
     serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsCreatorOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["status", "priority"]
     search_fields = ["title"]
@@ -23,4 +25,5 @@ class TaskViewSet(ModelViewSet):
         return serializer.save(created_by=self.request.user)
 
     def get_queryset(self):
-        return Task.objects.filter(created_by=self.request.user)
+        return (Task.objects.filter(Q(created_by=self.request.user) | Q(assigned_to=self.request.user)).
+                prefetch_related("assigned_to"))
