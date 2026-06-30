c14fb2e524b564d20197cf404ad937c6869bc4b7
Alireza <ali.r.matourianpoor@gmail.com>
2026-06-30 19:36:50 +0330

    add validate for is_read field in notificarion

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -13,3 +13,10 @@ class NotificationSerializer(serializers.ModelSerializer):
         model = Notification
         fields = ["id","recipient","title","message","is_read","created_at"]
         read_only_fields = ["id","recipient","title","message","created_at"]
+
+    def validate(self, attrs):
+        is_read = attrs["is_read"]
+        if self.instance:
+            if self.instance.is_read == True and is_read == False:
+                raise serializers.ValidationError({"is_read": "This field cannot be false"})
+        return attrs
