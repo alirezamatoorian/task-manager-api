b30c6d2ee5d11b9e9022cd9ec8f1355dd11eeca1
Alireza <ali.r.matourianpoor@gmail.com>
2026-07-06 19:09:47 +0330

    create admin panel for notification

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Notification
 
 # Register your models here.
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ["title","is_read","recipient","created_at"]
+    list_filter = ["is_read","recipient"]
+    ordering = ["-created_at"]
+    search_fields = ['recipient__phone']
