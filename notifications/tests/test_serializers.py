feaf2ce880c7f369532b8687f08481d59d157cd1
Alireza <ali.r.matourianpoor@gmail.com>
2026-07-23 17:47:44 +0330

    test serializer

--- notifications/tests/test_serializers.py
+++ notifications/tests/test_serializers.py
@@ -1,4 +1,14 @@
 import pytest
+
+from django.utils.timesince import timesince
 from notifications.serializers import NotificationSerializer
 
 pytestmark = pytest.mark.django_db
+
+
+def test_notification_serializer(notification):
+    serializer = NotificationSerializer(notification)
+    assert serializer.data["title"] == "welcome"
+    assert serializer.data["message"] == "Thanks for signing up"
+    assert serializer.data["is_read"] is False
+    assert serializer.data["created_at_human"]==timesince(notification.created_at)
